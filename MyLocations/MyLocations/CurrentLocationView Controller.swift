b667bf1742fb1ae3b711e5ee7b478f4496c84a0b
Art Williamson <alwilliamson@xactware.com>
2019-03-26 09:55:22 -0600

    added category table view controller and the ability to select a category for the location

--- MyLocations/MyLocations/CurrentLocationView Controller.swift
+++ MyLocations/MyLocations/CurrentLocationView Controller.swift
@@ -74,6 +74,13 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         navigationController?.isNavigationBarHidden = false
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+        }
+    }
     // MARK: - CLLocationManagerDelegate
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
